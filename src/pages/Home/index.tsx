d2fd6716e183c330aeae6a02a5e4fa85b1bfd993
Dion Fananie <dionfananie@gmail.com>
2024-11-23 16:20:30 +0700

    feat(init): example layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,14 @@
 // import { useLoaderData, Await } from "react-router-dom";
 // import { Suspense, useContext } from "react";
 
-import { Link } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
 import { loginUser } from "../../libs/auth";
 
 export function HomeRoute() {
+  const data = useOutletContext();
+  console.log("context outside private layout - user: ", data.user);
+  console.log("context outside private layout - auth: ", data.auth);
+
   return (
     <div>
       <h1>Home Page</h1>
